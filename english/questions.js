8885b500176ace820e631eeb5e59b4fca54ed2c2
bibekkumarchaudhary387 <bibekchaudhary2439@gmail.com>
2025-07-16 20:38:45 +0545

    add more quiz

---
+++ english/questions.js
@@ -0,0 +1,267 @@
+const Questions = [
+{
+  question: "What is the capital city of Nepal?",
+  options: ["Pokhara","Kathmandu","Bhaktapur","Lalitpur"],
+  answer: 1
+},
+{
+  question: "Which is the highest mountain in Nepal?",
+  options: ["Makalu","Dhaulagiri","Mount Everest","Annapurna"],
+  answer: 2
+},
+{
+  question: "How many districts are there in Nepal?",
+  options: ["72","77","78","75"],
+  answer: 1
+},
+{
+  question: "What is the longest river in Nepal?",
+  options: ["Bagmati","Koshi","Ghaghara(Karnali)","Gandaki"],
+  answer: 2
+},
+{
+  question: "Which is the largest lake in Nepal?",
+  options: ["Gosaikunda","Tilicho","Begnas","Rara"],
+  answer: 3
+},
+{
+  question: "What is the deepest gorge in Nepal?",
+  options: ["Seti Gorge","Kali Gandaki Gorge","Marsyangdi Gorge","Trishuli Gorge"],
+  answer: 1
+},
+{
+  question: "Which district is known as the 'Land of Oranges'?",
+  options: ["Gulmi","Ramechhap","Bhojpur","Salyan"],
+  answer: 1
+},
+{
+  question: "Which is the rainiest place in Nepal?",
+  options: ["Dharan","Pokhara","Lumle","Jiri"],
+  answer: 2
+},
+{
+  question: "Where is Rara Lake located?",
+  options: ["Kaski","Jumla","Mugu","Gorkha"],
+  answer: 2
+},
+{
+  question: "What is the name of the only international airport in Nepal before 2023?",
+  options: ["Bhairahawa Airport","Tribhuvan International Airport","Pokhara International","Gautam Buddha International"],
+  answer: 1
+},
+{
+  question: "Which two main seasons dominate Nepal?",
+  options: ["Summer and Autumn","Monsoon and Dry","Winter and Rainy","Spring and Autumn"],
+  answer: 1
+},
+{
+  question: "What is the name of the Terai's national park famous for one-horned rhinos?",
+  options: ["Bardiya National Park","Chitwan National Park","Banke National Park","Parsa Wildlife Reserve"],
+  answer: 1
+},
+{
+  question: "Which is the largest valley in Nepal?",
+  options: ["Kathmandu Valley","Pokhara Valley","Dang Valley","Tansen Valley"],
+  answer: 2
+},
+{
+  question: "Which region is famous for apple farming?",
+  options: ["Myagdi","Mustang","Gorkha","Taplejung"],
+  answer: 1
+},
+{
+  question: "What is the highest valley in the world?",
+  options: ["Manang Valley","Langtang Valley","Arun Valley","Rolpa Valley"],
+  answer: 2
+},
+{
+  question: "Which trekking route circles Mt. Annapurna?",
+  options: ["Everest Base Camp Trek","Lantang Trek","Annapurna Circuit","Manaslu Trek"],
+  answer: 2
+},
+{
+  question: "What is the Nepal's only desert-like area?",
+  options: ["Jumla","Upper Mustang","Humla","Darchula"],
+  answer: 1
+},
+{
+  question: "Which mountain is called the 'Killer Mountain'>",
+  options: ["Annapurna","Makalu","Nanga Parbat","Dhaulagiri"],
+  answer: 2
+},
+{
+  question: "What natural feature divides Nepla into 3 ecological zones?",
+  options: ["Climates","Forests","Elevation","Rivers"],
+  answer: 2
+},
+{
+  question: "Which river is considered the holiest in Nepal?",
+  options: ["Karnali","Bagmati","Seti","Arun"],
+  answer: 1
+},
+{
+  question: "Who was the first king of unified Nepal?",
+  options: ["Rajendra Bikram Shah","Prithvi Narayan Shah","Tribhuvan","Mahendra"],
+  answer: 1
+},
+{
+  question: "When did Nepal officially become a republic?",
+  options: ["B.S.2063","B.S.2064","B.S.2065","B.S.2066"],
+  answer: 2
+},
+{
+  question: "Who is known as the 'Father of the Nation' in Nepal?",
+  options: ["B.P. Koirala","Prithvi Narayan Shah","King Mahendra","Ganesh Man Singh"],
+  answer: 1
+},
+{
+  question: "In which year was the People's Movement II (Jana Andolan) held?",
+  options: ["B.S.2060-61","B.S.2061-62","B.S.2062-63","B.S.2063-64"],
+  answer: 2
+},
+{
+  question: "Who was the first democratically elected Prime Minister of Nepal?",
+  options: ["Girija Prasad Koirala","Krishna Prasad Bhattarai","Bishweshwar Prasad (B.P.) Koirala","Surya Bahadur Thapa"],
+  answer: 2
+},
+{
+  question: "When was the monarchy abolished in Nepal?",
+  options: ["B.S.2064","B.S.2065","B.S.2066","B.S.2067"],
+  answer: 1
+},
+{
+  question: "Who was the last king of Nepal?",
+  options: ["King Dipendra","King Mahendra","King Birendra","King Gyanendra"],
+  answer: 3
+},
+{
+  question: "What is the name of the interim constitution Nepal adopted after the 2006 movement?",
+  options: ["People's Constitution 2062","Peace Constitution","Interim Constitution of Nepal 2063","Transition Act 2063"],
+  answer: 2
+},
+{
+  question: "Who led the Maoist movement in Nepal?",
+  options: ["Baburam Bhattarai","Mohan Baidya","Ram Bahadur Thapa","Puspa Kamal Dahal (Prachanda)"],
+  answer: 3
+},
+{
+  question: "In which year was the first general election held in Nepal?",
+  options: ["B.S.2015","B.S.2018","B.S.2020","B.S.2021"],
+  answer: 0
+},
+{
+  question: "Which political party led the 1959 election victory?",
+  options: ["Communist Party of Nepal","Nepali Congress","Rastriya Prajatantra Party","Janamorcha Nepal"],
+  answer: 1
+},
+{
+  question: "Which treaty between Nepal and India signed in 1950?",
+  options: ["Sugauli Treaty","Treaty of Peace and Friendship","Border Agreement","Cultural Exchange Treaty"],
+  answer: 1
+},
+{
+  question: "Which king of Nepal introduced the Panchayat System?",
+  options: ["Tribhuvan","Mahendra","Birendra","Gyanendra"],
+  answer: 1
+},
+{
+  question: "Who was the Prime Minister of Nepal during the signing of the Comprehensive Peace Accord in 2006?",
+  options: ["Madhav Kumar Mandal","Girija Prasad Koirala","Pushpa Kamal Dahal","Sher Bahadur Deuba"],
+  answer: 1
+},
+{
+  question: "When did Nepal become a member of the United Nations?",
+  options: ["2008 BS","2012 BS","2014 BS","2016 BS"],
+  answer: 1
+},
+{
+  question: "Who was the first President of Nepal?",
+  options: ["Permanand Jha","Ram Baran Yadav","Bidhya Devi Bhandari","Girija Prasad Koirala"],
+  answer: 1
+},
+{
+  question: "When did Nepal adopt its current federal constitution?",
+  options: ["B.S.2071","B.S.2072","B.S.2073","B.S.2074"],
+  answer: 1
+},
+{
+  question: "How many provinces are there in federal Nepal?",
+  options: ["5","6","7","8"],
+  answer: 2
+},
+{
+  question: "Who was the first female President of Nepal?",
+  options: ["Sahana Pradhan","Bidhya Devi Bhandari","Onsari Gharti Magar","Shanta Chaudhary"],
+  answer: 1
+},
+{
+  question: "What is the minimum age requirment to be President of Nepal?",
+  options: ["30","35","40","45"],
+  answer: 3
+},
+{
+  question: "Who founded the Shah dynasty in Nepal?",
+  options: ["Prithvi Narayan Shah","Jung Bahadur Rana","Amar Singh Thapa","Araniko"],
+  answer: 0
+},
+{
+  question: "When was the Treaty of Sugauli signed?",
+  options: ["1968 BS","1913 BS","2021 BS","1856 BS"],
+  answer: 0
+},
+{
+  question: "Which queen commissioned the construction of Rani Pokhari in Kathmandu?",
+  options: ["Queen Bhuawana Rajya Lakshmi","Queen Rajendra Laxmi","Queen Tripurasundari","Queen Aishwarya"],
+  answer: 0
+},
+{
+  question: "The Lichchhavi period in Nepal is known for?",
+  options: ["Establishment of Gorkha Kingdom","Development of art and architecture","Unification of Nepal","Rana oligarchy"],
+  answer: 1
+},
+{
+  question: "When did the first democratic movement in Nepal start?",
+  options: ["2017 BS","2046 BS","2063 BS","2015 BS"],
+  answer: 1
+},
+{
+  question: "Who was the first Prime Minister of Nepal from the Rana regime?",
+  options: ["Bhimsen Thapa","Jung Bahadur Rana","Chandra Shumsher Rana","Mathabar Singh Thapa"],
+  answer: 1
+},
+{
+  question: "Which language is the official language of Nepal?",
+  options: ["Tharu","Newari","Nepali","Bhojpuri"],
+  answer: 2
+},
+{
+  question: "Who was the leader of the first democratic government of Nepal?",
+  options: ["Bishweshwar Prasad Koirala","Ganesh Man Singh","King Tribhuvan","Pushpa Kamal Dahal"],
+  answer: 0
+},
+{
+  question: "The major earthquake in Nepal occurred in which year BS?",
+  options: ["2070 BS","2072 BS","2065 BS","2068 BS"],
+  answer: 1
+},
+{
+  question: "The Pashupatinath Temple is dedicated to which deity?",
+  options: ["Vishnu","Ram","Shiva","Brahma"],
+  answer: 2
+},
+{
+  question: "The first ever Prime Minister of Nepal was?",
+  options: ["Ram Baran Yadav","Bhimsen Thapa","Jung Bahadur Rama","Mathabar Singh Thapa"],
+  answer: 1
+},
+{
+  question: "When did the People’s Movement I (Jana Andolan I) occur?",
+  options: ["2041 BS","2046 BS","2063 BS","2072 BS"],
+  answer: 1
+},
+{
+  question: "Which year did Nepal become a federal democratic republic?",
+  options: ["2064 BS","2065 BS","2072 BS","2063 BS"],
+  answer: 0
+},
+]
